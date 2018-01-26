e3c0268ecf57c95b7a2f7c71da900ca799df9495
Satendra Dagar <satendra.techmac@gmail.com>
2018-01-26 13:41:25 +0530

    added app icons, impleented file syncing feature

---
+++ Audio Capture/Audio Capture/ServerDataManagers/FileUploader.swift
@@ -0,0 +1,9 @@
+//
+//  FileUploader.swift
+//  Hello Demo
+//
+//  Created by Satendra Dagar on 23/01/18.
+//  Copyright © 2018 CB. All rights reserved.
+//
+
+import Foundation
---
+++ Audio Capture/Audio Capture/SyncFileManager/SyncFileManager.swift
@@ -0,0 +1,9 @@
+//
+//  SyncFileManager.swift
+//  Hello Demo
+//
+//  Created by Satendra Dagar on 16/01/18.
+//  Copyright © 2018 CB. All rights reserved.
+//
+
+import Foundation

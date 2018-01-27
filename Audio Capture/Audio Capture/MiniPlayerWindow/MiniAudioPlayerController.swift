9f82ab2223019e04a031a66ec1ac4bf3bd01db7d
Satendra Dagar <satendra.techmac@gmail.com>
2018-01-27 21:28:53 +0530

    Replaced the app icons

---
+++ Audio Capture/Audio Capture/MiniPlayerWindow/MiniAudioPlayerController.swift
@@ -0,0 +1,9 @@
+//
+//  MiniAudioPlayerController.swift
+//  Hello Demo
+//
+//  Created by Satendra Dagar on 26/01/18.
+//  Copyright © 2018 CB. All rights reserved.
+//
+
+import Foundation
---
+++ Audio Capture/Audio Capture/MiniPlayerWindow/PlayerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerViewController.swift
+//  Hello Demo
+//
+//  Created by Satendra Dagar on 26/01/18.
+//  Copyright © 2018 CB. All rights reserved.
+//
+
+import Foundation
